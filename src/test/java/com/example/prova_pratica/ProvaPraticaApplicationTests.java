cf58b6ae8d3b64095c2439260b14e10c55077cf2
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-01-03 19:21:10 -0300

    First commit

---
+++ src/test/java/com/example/prova_pratica/ProvaPraticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prova_pratica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaPraticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
