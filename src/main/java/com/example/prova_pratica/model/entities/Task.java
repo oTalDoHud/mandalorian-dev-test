0a9d1cadf9fee2d861dc61d6b13e83f47b7d4706
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-01-04 18:40:37 -0300

    Task size from 250 to 1500

--- src/main/java/com/example/prova_pratica/model/entities/Task.java
+++ src/main/java/com/example/prova_pratica/model/entities/Task.java
@@ -3,6 +3,7 @@ package com.example.prova_pratica.model.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class Task implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(length = 1500)
 	private String task;
 
 	public Task() {
