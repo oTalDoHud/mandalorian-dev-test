5739010743d402c07282fc0eedc4b3403cd59e0c
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-01-04 18:32:11 -0300

    update on service class

--- src/main/java/com/example/prova_pratica/services/TaskService.java
+++ src/main/java/com/example/prova_pratica/services/TaskService.java
@@ -1,5 +1,8 @@
 package com.example.prova_pratica.services;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,30 +23,20 @@ public class TaskService {
 		}
 		
 		String newTask = task.trim();
-		char[] especialChar = {'@', 'á', 'à', 'é', 'ã', '#'
-				, '!', '$', '%', '&', '*', '(', ')', '+', '='
-				, '-', '_', '/', '?', '°', 'è', 'ª', '[', ']'
-				, '{', '}', 'º', '^', '~', '´', '`', '\\', '|'
-				, ';', '.', ',', '>', '<', '¹', '²', '³', '£'
-				, '¢', '¬', '\'', '\"'};
-
+		
 		if (newTask.chars().count() < 10) {
 			Task t1 = new Task("Erro! Task have less 10 caracters");
 			return ResponseEntity.badRequest().body(t1);
 		}
-
-		for (int i = 0; i < newTask.length(); i++) {
-			for (int j = 0; j < especialChar.length; j++) {
-				
-				if (newTask.charAt(i) == especialChar[j]) {
-					Task t1 = new Task("Erro! Task have especial caracter '"
-							+ especialChar[j] + "' ");
-					return ResponseEntity.badRequest().body(t1);
-				}
-				
-			}
-
+		
+		Pattern p = Pattern.compile("[a-zA-Z0-9- ]{10,}");
+		Matcher task1 = p.matcher(task);
+		
+		if (!task1.matches()) {
+			Task t1 = new Task("Erro! Task have especial caracter");
+			return ResponseEntity.badRequest().body(t1);
 		}
+		
 		Task t1 = new Task(newTask);
 		taskRepo.save(t1);
 		return ResponseEntity.ok().body(t1);
